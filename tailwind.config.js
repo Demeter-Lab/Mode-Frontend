022c47980415799ced31676b493307ced6b9e19a
Mide001 <olamideisreal5@gmail.com>
2023-12-12 02:22:51 +0100

    added background color

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
   ],
   theme: {
     extend: {
+     // background: {linear-gradient(90deg,_#FFFFFF,_#000000)},
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
